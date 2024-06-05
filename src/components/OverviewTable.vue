f9896d030f0fd980c3273c7d51e570291643d84d
pincu <pincusjerome@gmail.com>
2024-06-05 13:00:25 +0200

    Import computed from vue

--- src/components/OverviewTable.vue
+++ src/components/OverviewTable.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
 interface Transaction {
   name: string;
