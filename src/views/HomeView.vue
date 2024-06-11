10015b6393412cf10430dffdc2bee66c2f99c56d
Louis Meyer-Breitländer <119730231+flabbaholt@users.noreply.github.com>
2024-06-11 19:15:32 +0200

    added totalBalance for current month request

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
 import TransactionForm from "@/components/TransactionForm.vue";
-import {ref} from "vue";
+import axios from 'axios';
+import {onMounted, ref} from "vue";
 
-const total = 350;
+const totalBalance = ref<number>(0);
 const currency = "EUR";
 
+async function fetchTotalBalance() {
+  try {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1; // Adding 1 because getMonth() is 0-based
+    console.log(currentYear, currentMonth);
+    const amountResponse = await axios.get(`${import.meta.env.VITE_APP_BACKEND_BASE_URL}/totalBalance/${currentYear}/${currentMonth}`);
+    totalBalance.value = amountResponse.data;
+  } catch (error) {
+    console.error("Error fetching total balance:", error);
+  }
+}
+
+onMounted(() => {
+  fetchTotalBalance();
+});
 </script>
 
 <template>
   <main>
     <h1 class="text-center fs-1">This Month:</h1>
-    <p :class="{'text-success': total >= 0, 'text-danger': total < 0}" class="text-center fs-2">{{ total < 0 ? '-' : '+' }}{{ Math.abs(total) }} {{ currency }}</p>
+    <p :class="{'text-success': totalBalance >= 0, 'text-danger': totalBalance < 0}" class="text-center fs-2">{{ totalBalance < 0 ? '-' : '+' }}{{ Math.abs(totalBalance) }} {{ currency }}</p>
     <TransactionForm />
 
   </main>
